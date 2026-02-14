790039c7e30d9d779a3178d7ccfe81c22b42a9d0
usmaanrnadeem <usmaanrnadeem@outlook.com>
2026-02-14 03:48:53 +0000

    Lol am now at home and can replace that rubbish with a simple list for now - every time a User object is created it is appended to the 'users' list

--- exchange.py
+++ exchange.py
@@ -1,14 +1,10 @@
 import math
 
-class IterRegistry(type):
-    def __iter__(cls):
-        return iter(cls._registry)
+users = []
 
 class User:
-    __metaclass__ = IterRegistry
-    _registry = []    
     def __init__(self, userID: int, username: str, yesPositions: dict[str, int]={}, noPositions: dict[str, int]={}, points: float=0):
-        self._registry.append(self)
+        users.append(self)
         self.userID = userID
         self.username = username
         self.points = points
