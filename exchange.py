78645b4b48a0b47f6dfe54cd7c3b9af2525ca677
usmaanrnadeem <usmaanrnadeem@outlook.com>
2026-02-14 03:55:49 +0000

    Extend the settlement logic to handle multiple users

--- exchange.py
+++ exchange.py
@@ -126,21 +126,3 @@ def LMSRCost(b: float, yesQuantity: int, noQuantity: int) -> float:
     expNo = math.exp(y - m)
 
     return b * (m + math.log(expYes + expNo))
-
-
-usmaan = User(1,'uz',{},{},100)
-
-usmaan2 = User(1,'uz2',{},{},100)
-
-election = Markets(1,20)
-
-election.buy(usmaan,10,1)
-election.buy(usmaan2,10,1)
-
-print(usmaan.points)
-print(usmaan2.points)
-
-election.settlement(0)
-
-print(usmaan.points)
-print(usmaan2.points)
